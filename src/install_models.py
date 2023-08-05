0d4500ab07be457eec379738afeff31542590ce0
Florian <70607158+Fdpr@users.noreply.github.com>
2023-08-05 03:40:14 +0200

    Update install_models.py

--- src/install_models.py
+++ src/install_models.py
@@ -1,10 +1,22 @@
 from transformers import pipeline
 import traceback
 
-models = ["stefan-it/german-gpt2-larger"]
+models = [
+    "stefan-it/german-gpt2-larger",
+    "malteos/bloom-6b4-clp-german",
+    "ai-forever/mGPT",
+    "DAMO-NLP-MT/polylm-13b",
+    "OpenBuddy/openbuddy-llama2-13b-v8.1-fp16",
+    "facebook/xglm-564M",
+    "facebook/xglm-1.7B",
+    "facebook/xglm-2.9B",
+    "facebook/xglm-4.5B",
+    "facebook/xglm-7.5B"
+]
 
 for model in models:
-    pipe = pipeline("text-generation", model = model)
+    print(f"Now loading model {model}")
+    pipe = pipeline("text-generation", model = model, framework = "pt")
     
     try:
         print(pipe("Ich bin ein Sprachmodell, also"))
