80286c45d425b1e542148140ab8cc8f0295c8970
Florian <70607158+Fdpr@users.noreply.github.com>
2023-08-07 04:50:39 +0200

    Update test_models.py

--- src/test_models.py
+++ src/test_models.py
@@ -15,7 +15,7 @@ models = [
 for model_name in models:
     print(f"Now loading model {model_name}")
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    force_flexible = ["Computer", "Technologie", "Linguistik"]
+    force_flexible = [" er"]
     try:
         force_words_ids = [
             tokenizer(force_flexible, add_prefix_space=True, add_special_tokens=False).input_ids,
@@ -27,6 +27,6 @@ for model_name in models:
         ]
     model = pipeline("text-generation", model = model_name)
     # print(model("Ich bin ein Sprachmodell, also", remove_invalid_values=True, early_stopping = True, do_sample = False, diversity_penalty = .6, num_beam_groups = 5, num_beams = 10, max_new_tokens = 20))
-    print(model("Ich bin ein Sprachmodell, also", remove_invalid_values=True, early_stopping = True, force_words_ids=force_words_ids, do_sample = False, num_beams = 10, max_new_tokens = 20))
+    print(model("Andra überraschte Malte, weil", remove_invalid_values=True, num_return_sequences = 1, no_repeat_ngram = 2, early_stopping = True, force_words_ids=force_words_ids, do_sample = False, num_beams = 10, max_new_tokens = 20))
     del model
     del tokenizer
