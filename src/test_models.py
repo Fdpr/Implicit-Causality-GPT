6b527a05da318c9372d57e568589f19af97b67de
Florian <70607158+Fdpr@users.noreply.github.com>
2023-08-07 04:36:58 +0200

    Update test_models.py

--- src/test_models.py
+++ src/test_models.py
@@ -26,6 +26,7 @@ for model_name in models:
             tokenizer(force_flexible, add_special_tokens=False).input_ids,
         ]
     model = pipeline("text-generation", model = model_name)
-    print(model("Ich bin ein Sprachmodell, also", remove_invalid_values=True, early_stopping = True, force_words_ids=force_words_ids, do_sample = False, diversity_penalty = .6, num_beam_groups = 5, num_beams = 10, max_new_tokens = 20))
+    # print(model("Ich bin ein Sprachmodell, also", remove_invalid_values=True, early_stopping = True, do_sample = False, diversity_penalty = .6, num_beam_groups = 5, num_beams = 10, max_new_tokens = 20))
+    print(model("Ich bin ein Sprachmodell, also", remove_invalid_values=True, early_stopping = True, force_words_ids=force_words_ids, do_sample = False, num_beams = 10, max_new_tokens = 20))
     del model
     del tokenizer
