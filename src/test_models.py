35b9a6d907450b4ad057e1405708a19d49f98c27
Florian <70607158+Fdpr@users.noreply.github.com>
2023-08-07 04:20:41 +0200

    diverse constrained beam search

--- src/test_models.py
+++ src/test_models.py
@@ -14,5 +14,10 @@ models = [
 
 for model_name in models:
     print(f"Now loading model {model_name}")
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    force_flexible = ["Computer", "Technologie", "Linguistik"] 
+    force_words_ids = [
+        tokenizer(force_flexible, add_prefix_space=True, add_special_tokens=False).input_ids,
+    ]
     model = pipeline("text-generation", model = model_name)
-    print(model("Ich bin ein Sprachmodell, also"))
+    print(model("Ich bin ein Sprachmodell, also"), do_sample = False, diversity_penalty = .6, num_beam_groups = 5, num_beams = 10, max_new_tokens = 20)
