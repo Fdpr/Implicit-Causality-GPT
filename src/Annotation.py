4563f291e708c0bcebace7139f33a0be49860098
Florian <70607158+Fdpr@users.noreply.github.com>
2023-08-09 11:49:02 +0200

    forced_reference

---
+++ src/Annotation.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+import spacy
+import urllib
+import warnings
+
+warnings.filterwarnings('ignore')
+nlp = spacy.load("de_dep_news_trf")
+
+
+# ### Felder für Annotation
+# - *text*: Von spacy analysiertes NLP-Objekt von Prompt + Fortsetzung
+# - *prompt*: Slice von *text*, der nur den Prompt umfasst
+# - *cont*: Slice von *text*, der nur die Fortsetzung umfasst
+# - *verbclass*: Wenn vorhanden, Klasse des Verbs: es (experiencer-stimulus), se (stimulus-experiencer)
+# - *NP1*: Name von NP1
+# - *NP2*: Name von NP2
+# - *NP1gender*: Geschlecht von NP1: m oder f
+
+# #### Anaphorische Koreferenz
+
+def coreference(item):
+    if item["cont"][0].tag_ == "KOUI":
+        return ("NP1", "elliptisch")
+    try:
+        subject = next(token for token in item["cont"] if token.dep_ == "sb")
+        form = ""
+        coreference = ""
+        if subject.text.lower() in ["er", "sie"]:
+            form = "personal"
+        elif subject.text.lower() in ["dieser", "diese"]:
+            form = "demonstrativ"
+        elif subject.text.lower() in [item["NP1"].lower(), item["NP2"].lower()]:
+            form = "Eigenname"
+        elif subject.text.lower() in ["der", "welcher", "die", "welche"]:
+            form = "relativ"
+        if form != "":
+            if "Plur" in subject.morph.get("Number"):
+                coreference = "NP1+NP2"
+            else:
+                if "Masc" in subject.morph.get("Gender"):
+                    if item["NP1gender"] == "m":
+                        coreference = "NP1"
+                    else:
+                        coreference = "NP2"
+                elif "Fem" in subject.morph.get("Gender"):
+                    if item["NP1gender"] == "f":
+                        coreference = "NP1"
+                    else:
+                        coreference = "NP2"
+        return (coreference, form)
+    except StopIteration:
+        return ("", "")
+
+
+
+def relative_coreference(item):
+    reltest = item["cont"][0]
+    if reltest.tag_ == "PRELS" or reltest.tag_ == "PRELAT":
+        if "Plur" in reltest.morph.get("Number"):
+            return "NP1+NP2"
+        if "Masc" in reltest.morph.get("Gender"):
+            if item["NP1gender"] == "f":
+                return "NP2"
+            else:
+                return "NP1"
+        elif "Fem" in reltest.morph.get("Gender"):
+            if item["NP1gender"] == "f":
+                 return "NP1"
+            else:
+                return "NP2"
+    return ""
+
+
+# #### Fortsetzungstyp
+
+# In[4]:
+
+
+def cont_type(item):
+    cont = item["cont"][0]
+    tag = cont.tag_
+    if tag == "PRELS" or tag == "PRELAT":
+        return "Relativsatz"
+    elif tag == "APPR" or tag == "APPRART":
+        try:
+            subject_index = next(token for token in item["cont"] if token.dep_ == "sb").i
+            verb_index = next(token for token in item["cont"] if "Fin" in token.morph.get("VerbForm")).i
+            return "Hauptsatz"
+        except StopIteration:
+            pass
+        return "PP"
+    elif tag == "KOUS" or tag == "KOUI":
+        return "subordinierend"
+    elif tag == "KON":
+        return "koordinierend"
+    else:
+        try:
+            subject_index = next(token for token in item["cont"] if token.dep_ == "sb").i
+            verb_index = next(token for token in item["cont"] if "Fin" in token.morph.get("VerbForm")).i
+            return "Hauptsatz"
+        except StopIteration:
+            pass
+    return ""
+
+
+# #### Diskursrelation
+
+# In[5]:
+
+
+discourse_markers = {}
+explanation = ("weil ", "aufgrund dessen ", "da ", "dadurch, dass ", "denn ", "ergo ", "in Anbetracht dessen ", "insofern, als ", "zumal ") 
+discourse_markers[explanation] = "Contingency.Cause.Reason"
+consequence = ("sodass ", "also ", "daher ", "darum ", "dementsprechend ", "demgemäß ", "demnach ", "demzufolge ", "deshalb ", "deswegen ", "drum ", "folgerichtig ", "folglich ", "infolgedessen ", "sonach ", "weshalb ", "weswegen ", "wodurch ") 
+discourse_markers[consequence] = "Contingency.Cause.Result"
+occasion = ("bevor ", "ehe ", "wonach ", "worauf ", "woraufhin ") 
+discourse_markers[occasion] = "Temporal.Asynchronous.Precedence"
+elaboration = ("und zwar ", "beziehungsweise ", "genauer gesagt ", "nämlich ") 
+discourse_markers[elaboration] = "Expansion.Level-of-Detail.Arg2-as-detail"
+contrast = ("aber ", "allerdings ", "andererseits ", "dafür ", "dagegen ", "dahingegen ", "demgegenüber ", "dem gegenüber ", "doch ", "hingegen ", "jedoch ", "nur dass ", "wogegen ", "wohingegen ", "wohin gegen ") 
+discourse_markers[contrast] = "Comparison.Contrast"
+synchronous = ("als ", "derweilen ", "gleichzeitig ", "nebenbei ", "nebenher ", "nebenbei ", "während ", "währenddessen ", "unterdessen ", "zugleich ")
+discourse_markers[synchronous] = "Temporal.Synchronous"
+succession = ("davor ", "kaum dass ", "nachdem ", "seitdem ", "vorher ", "zuerst ", "zunächst ", "zuvor ")
+discourse_markers[succession] = "Temporal.Asynchronous.Succession"
+concession = ("obwohl ", "gleichwohl ", "obgleich ", "obschon ", "obzwar ", "trotzdem ", "wenngleich ", "zwar ")
+discourse_markers[concession] = "Comparison.Concession"
+manner = ("als wenn ", "dadurch, dass ", "indem ")
+discourse_markers[manner] = "Expansion.Manner.Arg2-as-manner"
+goal = ("damit ", "um ")
+discourse_markers[goal] = "Contingency.Purpose.Arg2-as-goal"
+
+def discourse_relation_explicit(item):
+    marker = item["cont"][0]
+    if marker.tag_ in ["KOUI", "KON", "KOUS"]:
+        cont_l = item["cont"].text.lower()
+        for marker_list in discourse_markers.keys():
+            if any(cont_l.startswith(marker) for marker in marker_list):
+                return discourse_markers[marker_list]
+    return ""
+
+def discourse_relation_explicit_corrected(item):
+    marker = item["cont"][0].text.lower()
+    if "verbclass" in item.keys():
+        cat = item["verbclass"]
+        if (marker == "indem" or marker == "als") and cat == "se":
+            return "Contingency.Cause.Reason"
+        elif (marker == "der" or marker == "die" or marker == "welcher" or marker == "welche") and (cat == "se" or cat == "es"):
+            return "Contingency.Cause.Reason"
+    return discourse_relation_explicit(item)
+
+def discourse_relation_implicit(item):
+    pass
+
+
+# # Annotation
+# # DO NOT USE FOR GENERAL ANNOTATION. CHANGES HAVE BEEN MADE FOR FORCED REFERENCE AND STUFF!!!!!!!!
+# 
+# Folgendes wird annotiert:
+# 
+# - **Diskursrelation Explizit**: Erkennt ausgewählte PDTB-Relationen anhand des Konnektors nach dem Komma.
+# - **Diskursrelation Explizit SE/ES-Korrektur**: Wenn Informationen über ES/SE-Kategorie des Verbs vorhanden sind, wird die explizite Annotation für manche Konnektoren angepasst. Zum Beispiel: als -> Synchronous bei ES-Verben, aber als -> Reason bei SE-Verben
+# - **Diskursrelation Implizit**: TODO, Erkennung von Diskursrelationen mittels BERT-Klassifizierung
+# - **Fortsetzungstyp**: Die syntaktische Art der Fortsetzung: PP, Rel-Satz, subordinierend, koordinierend, Hauptsatz
+# - **Relativkoreferenz**: Bei Relativsätzen: NP1- oder NP2-Koreferenz des Relativpronomens
+# - **Koreferenz**: Markiert anaphorische Koreferenz zu NP1, NP2 oder NP1+NP2, so vorhanden
+# - **Anaphorische Form**: Wenn Koreferenz: Form der Anapher: personal, demonstrativ, relativ, Eigenname, elliptisch
+
+# In[6]:
+
+
+# has_connector: Enthält der Prompt einen Konnektor? -> Nur Koreferenz und anaphorische Form annotieren
+def annotate(row, has_connector = False):
+    if row["type"] != "Experiment":
+        return {}
+    text = nlp(row["prompt"] + " " + row["cont"])
+    weil = next(token for token in text if token.text == "weil").i
+    prompt = text[:weil]
+    cont = text[weil:]
+    item = {"text": text, "prompt": prompt, "cont":cont, "NP1": row["NP1"], "NP2": row["NP2"],"NP1gender": row["NP1gender"]}
+    if "verbclass" in row.keys():
+        item["verbclass"] = row["verbclass"]
+    result = dict()
+    result["Koreferenz"], result["Anaphorische Form"] = coreference(item)
+    if not has_connector:
+        result["Diskursrelation Explizit"] = discourse_relation_explicit(item)
+        if "verbclass" in row.keys():
+            item["verbclass"] = row["verbclass"]
+            result["Diskursrelation Explizit SE/ES-Korrektur"] = discourse_relation_explicit_corrected(item)
+        result["Diskursrelation Implizit"] = discourse_relation_implicit(item)
+        result["Fortsetzungstyp"] = cont_type(item)
+        result["Relativkoreferenz"] = relative_coreference(item)
+    return result
+
+
+
+def do_annotation(df, has_connector = False):
+    """
+    Datenschema
+    
+    Folgende Spalten muss die zu annotierende Datei umfassen:
+    
+    - *type*: Nur wenn type = "Experiment" ist, wird die Zeile annotiert.
+    - *prompt*: Enthält Prompt bis einschließlich Komma / Konnektor (ohne Leerzeichen). Wenn Kontext vorhanden, muss der vorher entfernt werden. Prompt ist also nur der Prompt-Satz ohne vorhergehende Kontextsätze
+    - *cont*: Enthält Fortsetzung nach dem Komma (ohne Leerzeichen vor dem ersten Wort)
+    - *verbclass*: Wenn vorhanden, Klasse des Verbs: es (experiencer-stimulus), se (stimulus-experiencer)
+    - *NP1*: Name von NP1
+    - *NP2*: Name von NP2
+    - *NP1gender*: Geschlecht von NP1: m oder f
+    
+    has_connector: Enthält der Prompt einen Konnektor? -> Nur Koreferenz und anaphorische Form annotieren
+    """
+    
+    index = df.index
+    res = pd.DataFrame.from_records(list(df.apply(lambda row: annotate(row, has_connector), axis=1)))
+    res.index = df.index
+    return pd.concat([df, res], axis=1)
+
+
---
+++ src/forcedreference_generate.py
@@ -0,0 +1,126 @@
+from transformers import pipeline, AutoTokenizer
+import pandas as pd
+import json
+from random import Random
+from torch.utils.data import Dataset
+from tqdm import tqdm
+import transformers
+import traceback
+from Annotation import annotate
+from copy import deepcopy
+
+transformers.logging.set_verbosity_error()
+
+ITEMS_PER_CONDITION = 10
+
+class PromptDataset(Dataset):
+    def __init__(self, prompts):
+        self.prompts = prompts
+    def __len__(self):
+        return len(self.prompts)
+    def __getitem__(self, idx):
+        return self.prompts[idx]
+        
+
+models = [
+    # model name, batch_size, device, device Mapping
+    ("stefan-it/german-gpt2-larger", 64, 0, None),
+    # ("malteos/bloom-6b4-clp-german", 1, 0, None), #Bloom is too big
+    ("ai-forever/mGPT", 4, 0, None),
+    ("facebook/xglm-564M", 16, 0, None),
+    ("facebook/xglm-1.7B", 4, 0, None),
+    # ("facebook/xglm-2.9B", 16, -1, None), # The larger models should be ran on a bigger GPU
+    # ("facebook/xglm-4.5B", 1, -1, "auto") # The larger models should be ran on a bigger GPU
+]
+
+with open("../items/names.json", encoding="utf-8") as nfile:
+    namedict = json.load(nfile)
+male_names = [name for name in namedict["male"]] * 2
+female_names = [name for name in namedict["female"]] * 2
+with open("../items/verbs_forced_reference.json", encoding="utf-8") as nfile:
+    verbdict = json.load(nfile)
+es_verbs = verbdict["es"]
+se_verbs = verbdict["se"]
+
+female_shuffled = female_names.copy()
+Random(42).shuffle(female_shuffled)
+male_shuffled = male_names.copy()
+Random(84).shuffle(male_shuffled)
+male_pairing = list(zip(male_names, female_shuffled, [False for name in male_names]))
+female_pairing = list(zip(female_names, male_shuffled, [True for name in male_names]))
+
+conditions = [
+    (2,  es_verbs, female_pairing, "NP1"),
+    (3,  es_verbs,   male_pairing, "NP1"),
+    (6,  se_verbs, female_pairing, "NP1"),
+    (7,  se_verbs,   male_pairing, "NP1"),
+    (10, es_verbs, female_pairing, "NP2"),
+    (11, es_verbs,   male_pairing, "NP2"),
+    (14, se_verbs, female_pairing, "NP2"),
+    (15, se_verbs,   male_pairing, "NP2")
+]
+
+items_per_condition = []
+  
+for condition, verbs, pairing, forced_reference in conditions:
+    rows = []
+    for verbdict in verbs:
+        verb, filler, verbclass = verbdict["verb"], verbdict["filler"], verbdict["verbclass"]
+        for np1, np2, female in pairing:
+            prompt = f"{np1} {verb} {np2}{filler}, weil"
+            nrow = {"condition": condition, "type": "Experiment", "prompt": prompt, "NP1": np1, "NP2": np2, 
+                    "NP1gender": "f" if female else "m", "verb": verb, "verbclass": verbclass, "forced": forced_reference}
+            rows.append(nrow)
+    Random(168).shuffle(rows)
+    items_per_condition.append(rows)
+
+for model_name, batch_size, device, device_map in models:
+       
+    print(f"now loading: {model_name}")
+    model = pipeline("text-generation", model = model_name, device = device, device_map = device_map)
+    model.tokenizer.pad_token_id = model.model.config.eos_token_id
+    model.tokenizer.padding_side = "left"
+    
+    male_tokens = list(map(model.tokenizer.encode, [" er", " dieser", " jener", " der"]))
+    female_tokens = list(map(model.tokenizer.encode, [" sie", " diese", " jene", " die"]))
+    
+    data = []
+
+    for condition in items_per_condition:
+        items = deepcopy(condition)
+        bar = tqdm(total = ITEMS_PER_CONDITION)
+        item_iter = iter(items)
+        rows = []
+        while bar.n < ITEMS_PER_CONDITION:
+            try:
+                row = next(item_iter)
+                if row["forced"] == "NP1":
+                    tokenized_name = model.tokenizer.encode(f" {row['NP1']}")
+                else:
+                    tokenized_name = model.tokenizer.encode(f" {row['NP2']}")
+                if (row["NP1gender"] == "m" and row["forced"] == "NP1") or (row["NP1gender"] == "f" and row["forced"] == "NP2"):
+                    forced_tokens = male_tokens + [tokenized_name]
+                else:
+                    forced_tokens = female_tokens + [tokenized_name]
+                continuation = model(row["prompt"], force_words_ids = [forced_tokens], remove_invalid_values=True, early_stopping = True, do_sample = False, num_beams = 10, max_new_tokens = 25)[0]["generated_text"]
+                row["cont"] = continuation[len(row["prompt"]) + 1:]
+                res = annotate(row, False)
+                if res["Koreferenz"] == row["forced"]:
+                    bar.update(1)
+                    row.update(res)
+                    rows.append(row)
+            except StopIteration:
+                print(f"Run out of data in condition {items[0]['condition']}")
+                break
+        data += rows
+
+    exp3 = pd.DataFrame(data, columns = ["condition", "type", "prompt", "cont", "NP1", "NP2", "NP1gender", "verb", "verbclass", "forced", "Koreferenz", "Anaphorische Form"])
+    exp3.to_csv(f"../data/forced_coreference--{model_name.replace('/', '--')}.csv", sep=";", index=False)
+    
+    del model
+    del exp3
+    del rows
+    del items
+    
+    
+    # FIX ROW BUG
