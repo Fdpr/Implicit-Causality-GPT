c559fbd5d9eda5a04528add476be719c4999a1d1
Florian <70607158+Fdpr@users.noreply.github.com>
2024-03-11 22:10:26 +0100

    human annotation

---
+++ src/annotate_humans.py
@@ -0,0 +1,19 @@
+from Annotation import do_annotation
+import Annotation_forced
+from os import listdir
+import pandas as pd
+
+for result_file in [file for file in listdir("../humans") if file.startswith("exp_1") and file.endswith(".csv")]:
+    result_file = "../humans/" + result_file
+    print(f" Now annotating {result_file}")
+    do_annotation(df, True).to_csv(result_file[:-4] + "_annotated.csv", index=False, sep=";") 
+    
+for result_file in [file for file in listdir("../humans") if file.startswith("exp_2") and file.endswith(".csv")]:
+    result_file = "../humans/" + result_file
+    print(f" Now annotating {result_file}")
+    do_annotation(df, False).to_csv(result_file[:-4] + "_annotated.csv", index=False, sep=";") 
+    
+for result_file in [file for file in listdir("../humans") if file.startswith("exp_3") and file.endswith(".csv")]:
+    result_file = "../humans/" + result_file
+    print(f" Now annotating {result_file}")
+    Annotation_forced.do_annotation(df, True).to_csv(result_file[:-4] + "_annotated.csv", index=False, sep=";") 
