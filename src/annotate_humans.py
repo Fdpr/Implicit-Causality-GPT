5f242e826e0b383e6b79c52406555345e68db3b7
Florian <70607158+Fdpr@users.noreply.github.com>
2024-03-11 22:13:35 +0100

    fix human

--- src/annotate_humans.py
+++ src/annotate_humans.py
@@ -6,14 +6,17 @@ import pandas as pd
 for result_file in [file for file in listdir("../humans") if file.startswith("exp_1") and file.endswith(".csv")]:
     result_file = "../humans/" + result_file
     print(f" Now annotating {result_file}")
+    df = pd.read_csv(result_file, sep=";")
     do_annotation(df, True).to_csv(result_file[:-4] + "_annotated.csv", index=False, sep=";") 
     
 for result_file in [file for file in listdir("../humans") if file.startswith("exp_2") and file.endswith(".csv")]:
     result_file = "../humans/" + result_file
     print(f" Now annotating {result_file}")
+    df = pd.read_csv(result_file, sep=";")
     do_annotation(df, False).to_csv(result_file[:-4] + "_annotated.csv", index=False, sep=";") 
     
 for result_file in [file for file in listdir("../humans") if file.startswith("exp_3") and file.endswith(".csv")]:
     result_file = "../humans/" + result_file
     print(f" Now annotating {result_file}")
+    df = pd.read_csv(result_file, sep=";")
     Annotation_forced.do_annotation(df, True).to_csv(result_file[:-4] + "_annotated.csv", index=False, sep=";") 
