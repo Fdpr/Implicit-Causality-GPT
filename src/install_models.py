1cafe1279d89e57d93c2ae648ea2a5df1cbafc53
Florian <70607158+Fdpr@users.noreply.github.com>
2024-03-14 00:03:35 +0100

    new models installer

--- src/install_models.py
+++ src/install_models.py
@@ -1,76 +1,52 @@
-from transformers import AutoConfig, pipeline, AutoTokenizer, LlamaTokenizer, AutoModelForCausalLM
-from accelerate import init_empty_weights, load_checkpoint_and_dispatch
-from pathlib import Path
-
-HOME = str(Path.home())
-
-import traceback
+from transformers import pipeline
+from torch.utils.data import Dataset
+from tqdm import tqdm
 
 models = [
-    ("stefan-it/german-gpt2-larger",
-    "/.cache/huggingface/hub/models--stefan-it--german-gpt2-larger/snapshots/aa2138bb716507181c1bbd288a1076837ed0ca3b/pytorch_model.bin",
-    AutoTokenizer,
-    True
-    ),
-    ("malteos/bloom-6b4-clp-german",
-    "/.cache/huggingface/hub/models--malteos--bloom-6b4-clp-german/snapshots/2e5a85761d8a39ec602ada390e87ff8d5d316191/pytorch_model.bin.index.json",
-    AutoTokenizer,
-    False),
-    ("ai-forever/mGPT",
-    "/.cache/huggingface/hub/models--ai-forever--mGPT/snapshots/cb99dd42d3486c2ed9c14f92ed22e9bc7bbc6ac4/pytorch_model.bin",
-    AutoTokenizer,
-    True),
-    # For some reason, the polylm model doesn't work with Accelerate
-    # ("DAMO-NLP-MT/polylm-13b",
-    # "/.cache/huggingface/hub/models--DAMO-NLP-MT--polylm-13b/snapshots/685e5014840342bfdecd55ea961d435357c16a22/pytorch_model.bin.index.json",
-    # LlamaTokenizer,
-    # True),
-    # We don't to open buddy for now, sadly...
-    # ("OpenBuddy/openbuddy-llama2-13b-v8.1-fp16",
-    # "/.cache/huggingface/hub/models--OpenBuddy--openbuddy-llama2-13b-v8.1-fp16/snapshots/c1a5f8fd23e3823ced6efc0ea1df973da850ba7e/pytorch_model.bin.index.json",
-    # AutoTokenizer,
-    # True),
-    ("facebook/xglm-564M",
-    "/.cache/huggingface/hub/models--facebook--xglm-564M/snapshots/f3059f01b98ccc877c673149e0178c0e957660f9/pytorch_model.bin",
-    AutoTokenizer,
-    True),
-    ("facebook/xglm-1.7B",
-    "/.cache/huggingface/hub/models--facebook--xglm-1.7B/snapshots/d23a5e8e2164af31a84a26756b9b17f925143050/pytorch_model.bin",
-    AutoTokenizer,
-    True),
-    ("facebook/xglm-2.9B",
-    "/.cache/huggingface/hub/models--facebook--xglm-2.9B/snapshots/33c659ae27de09c0a54123d3902dac48cbb8592a/pytorch_model.bin",
-    AutoTokenizer,
-    True),
-    ("facebook/xglm-4.5B",
-    "/.cache/huggingface/hub/models--facebook--xglm-4.5B/snapshots/dc6a67fac06c8bca7860b84656a0cb736293a7a8/pytorch_model.bin",
-    AutoTokenizer,
-    True),
-    # The 7.5B model is not sharded and won't work with Accelerate
-    # ("facebook/xglm-7.5B",
-    # "/.cache/huggingface/hub/models--facebook--xglm-7.5B/snapshots/732d59308a844004bd9a4def972cc7c3896a38e0/pytorch_model.bin",
-    # AutoTokenizer,
-    # True)
+    # model name, batch_size, device, device Mapping
+    # ("stefan-it/german-gpt2-larger", 64, 0, None),
+    # ("ai-forever/mGPT", 4, 0, None),
+    # ("facebook/xglm-564M", 16, 0, None),
+    # ("facebook/xglm-1.7B", 4, 0, None),
+    ("facebook/xglm-2.9B", 16, -1, None), # The larger models should be ran on a bigger GPU 
+    ("facebook/xglm-4.5B", 1, -1, None), # The larger models should be ran on a bigger GPU
+    ("facebook/xglm-7.5B", 1, -1, None), # The larger models should be ran on a bigger GPU
+    # ("malteos/bloom-350m-german", 32, 0, None),
+    # ("malteos/bloom-1b5-clp-german", 4, 0, None)
+    ("malteos/bloom-6b4-clp-german", 1, 0, None) # The larger models should be ran on a bigger GPU
 ]
 
-for model_name, local_path, tokenizer, use_accelerate in models:
-    print(f"Now loading model {model_name}")
+class MyDataset(Dataset):
+    def __len__(self):
+        return 64
+
+    def __getitem__(self, i):
+        return "Peter hasste Maria ganz ohne Absicht, weil"
+
+for model in models:
+    pipe = pipeline("text-generation", model = model, device = 0)
     
-    if use_accelerate:  
-        config = AutoConfig.from_pretrained(model_name)
-        with init_empty_weights():
-            model = AutoModelForCausalLM.from_config(config)
-        model.tie_weights()
-        model = load_checkpoint_and_dispatch(model, HOME + local_path, device_map="auto", offload_folder = ".temp")
-    else:
-        model = AutoModelForCausalLM.from_pretrained(model_name)
-    tokenizer = tokenizer.from_pretrained(model_name)
+for model in models:
+    print(f"model {model} with batch size 16"
+    pipe = pipeline("text-generation", model = model, device = 0)
+    dataset = MyDataset()
+    tqdm(pipe(dataset, batch_size=16, remove_invalid_values=True, early_stopping = True, do_sample = False, diversity_penalty = .6, num_beam_groups = 10, num_beams = 10, max_new_tokens = 25), total=len(dataset))
     
-    try:
-        inputs = tokenizer("Ich bin ein Sprachmodell, also", return_tensors="pt")
-        print(tokenizer.decode(model.generate(**inputs)[0]))
-    except:
-        traceback.print_exc()
-    finally:
-        del model
-        del tokenizer
+for model in models:
+    print(f"model {model} with batch size 32"
+    pipe = pipeline("text-generation", model = model, device = 0)
+    dataset = MyDataset()
+    tqdm(pipe(dataset, batch_size=32, remove_invalid_values=True, early_stopping = True, do_sample = False, diversity_penalty = .6, num_beam_groups = 10, num_beams = 10, max_new_tokens = 25), total=len(dataset))
+
+for model in models:
+    print(f"model {model} with batch size 64"
+    pipe = pipeline("text-generation", model = model, device = 0)
+    dataset = MyDataset()
+    tqdm(pipe(dataset, batch_size=64, remove_invalid_values=True, early_stopping = True, do_sample = False, diversity_penalty = .6, num_beam_groups = 10, num_beams = 10, max_new_tokens = 25), total=len(dataset))
+
+for model in models:
+    print(f"model {model} with batch size 128"
+    pipe = pipeline("text-generation", model = model, device = 0)
+    dataset = MyDataset()
+    tqdm(pipe(dataset, batch_size=128, remove_invalid_values=True, early_stopping = True, do_sample = False, diversity_penalty = .6, num_beam_groups = 10, num_beams = 10, max_new_tokens = 25), total=len(dataset))
+
